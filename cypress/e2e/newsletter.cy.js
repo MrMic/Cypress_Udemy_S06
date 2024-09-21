5b96a227dd6da7f0c17d2f848d0de132d571d97d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-21 15:43:39 +0200

    feat: Another DB dependent test

---
+++ cypress/e2e/newsletter.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Newsletter', () => {
+  beforeEach(() => {
+    cy.task('seedDatabase');
+  })
+
+  it('should display a success message', () => {
+    cy.visit('/');
+    cy.wait(500).get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.contains("Thanks for signing up");
+  });
+});
