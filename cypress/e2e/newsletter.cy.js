a1742943bad8bba3c718574411b70d82c45a5b5c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-22 12:23:49 +0200

    feat: Use Interceptors

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -6,9 +6,12 @@ describe('Newsletter', () => {
   })
 
   it('should display a success message', () => {
+    // * INFO: Will intercept POST requests to /newsletter?anything
+    cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
     cy.visit('/');
-    cy.wait(500).get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.wait(50).get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
     cy.contains("Thanks for signing up");
   });
 });
