925b43a26b75b79aa1e89cab72770793d2b04185
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-26 16:29:05 +0200

    feat: More Intercepting

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -9,9 +9,19 @@ describe('Newsletter', () => {
     // * INFO: Will intercept POST requests to /newsletter?anything
     cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
     cy.visit('/');
-    cy.wait(50).get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.wait(500).get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.wait('@subscribe');
     cy.contains("Thanks for signing up");
   });
+
+  it('should display validation error', () => {
+    // * INFO: Will intercept POST requests to /newsletter?anything
+    cy.intercept('POST', '/newsletter*', { message: 'Email exists already.' }).as('subscribe');
+    cy.visit('/');
+    cy.wait(50).get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains("Email exists already.");
+  })
 });
