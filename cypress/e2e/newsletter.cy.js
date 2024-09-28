d08b8bb6085837090608f29ed687eb119548aed2
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-28 14:27:24 +0200

    feat: Test APIs w/ Test Request

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -19,9 +19,22 @@ describe('Newsletter', () => {
     // * INFO: Will intercept POST requests to /newsletter?anything
     cy.intercept('POST', '/newsletter*', { message: 'Email exists already.' }).as('subscribe');
     cy.visit('/');
-    cy.wait(50).get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.wait(500).get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.wait('@subscribe');
     cy.contains("Email exists already.");
-  })
+  });
+
+  it('should successfully create a new contact', () => {
+    cy.request({
+      method: 'POST',
+      url: '/newsletter',
+      form: true,
+      body: {
+        email: 'test@example.com'
+      }
+    }).then(response => {
+      expect(response.status).to.eq(201);
+    });
+  });
 });
