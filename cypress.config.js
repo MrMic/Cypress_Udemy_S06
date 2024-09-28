feeeed2db10b46b9eb1f26e48db21629a017fb3a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-28 14:27:24 +0200

    feat: Test APIs w/ Test Request

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ import { seed } from './prisma/seed-test';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3002',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('task', {
